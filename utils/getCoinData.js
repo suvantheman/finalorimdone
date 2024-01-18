29bb10beac36f130bb186b0aeaa7273917569cfa
Luacrative <afif317@gmail.com>
2024-01-17 21:00:36 -0800

    Refactoring systems

---
+++ utils/getCoinData.js
@@ -0,0 +1,34 @@
+const headers = ["Date", "Open", "High", "Low", "Close", "Adj", "Volume"];
+
+const processCSV = csv => {
+    const lines = [];
+
+    csv.split("\n").forEach(line => {
+        const data = {};
+        const values = line.split(",");
+
+        for (let i = 0; i < values.length; i++)  
+            data[headers[i]] = values[i];
+
+        lines.push(data);
+    });
+
+    return lines;  
+};
+
+const getCSVData = async csv => {
+    const response = await fetch(`../assets/stock-data/${csv}.txt`);
+    const data = await response.text();
+    const points = processCSV(data);
+
+    return points;
+};
+
+const getCoinData = async (coin, start) => {
+    const data = await getCSVData(coin);
+    const lines = data.length; 
+
+    return data.slice(lines - start, lines);
+}
+
+export default getCoinData;
